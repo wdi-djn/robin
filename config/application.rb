bbe97f04947ca84645fa66d5221da59f2c57649a
Neil Spurgeon <neilspurgeon@gmail.com>
2015-06-21 11:47:17 -0700

    setup susy grid

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'susy'
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Hippopotamus
+module HippoApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
