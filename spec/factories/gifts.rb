51a814e37113214968905230cb2ae42845f64b1c
jmbrenne <jeremybrenner0@gmail.com>
2015-06-22 10:13:59 -0700

    Added gift factory

--- spec/factories/gifts.rb
+++ spec/factories/gifts.rb
@@ -1,6 +1,25 @@
+ # create_table "gifts", force: :cascade do |t|
+ #    t.string   "title"
+ #    t.text     "description"
+ #    t.decimal  "price"
+ #    t.string   "gift_url"
+ #    t.datetime "due_date"
+ #    t.string   "recipient"
+ #    t.integer  "user_id"
+ #    t.string   "hashed_id"
+ #    t.datetime "created_at",  null: false
+ #    t.datetime "updated_at",  null: false
+ #  end
+
 FactoryGirl.define do
   factory :gift do
-    
+  	title { FFaker::Lorem.sentence(5)}
+  	description { FFaker::Lorem.paragraph}
+  	price { FFaker::}
+  	gift_url { FFaker::}
+  	due_date { FFaker::}
+  	recipient { FFaker::}
+  	user_id { FFaker::}
+  	hashed_id { FFaker::}
   end
-
 end
