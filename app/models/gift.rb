1374bb7f7d785441988a00800461d595cb813087
jmbrenne <jeremybrenner0@gmail.com>
2015-06-24 13:23:03 -0700

    Modified models and tests

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -16,7 +16,7 @@ class Gift < ActiveRecord::Base
   
   validates :recipient, 
   	presence: true,
-  	:length => { maximum: 20 }
+  	:length => { maximum: 50 }
 
   validates :gift_url, 
   	format: { with: /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?\z/ix, 
