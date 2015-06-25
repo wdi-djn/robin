5a3a74860a6571d84923395e4fe05399de10c29e
danlyg <gih.daniel@gmail.com>
2015-06-24 23:02:44 -0700

    added can_fund column to gifts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150625041225) do
+ActiveRecord::Schema.define(version: 20150625055848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,11 +37,12 @@ ActiveRecord::Schema.define(version: 20150625041225) do
     t.string   "recipient"
     t.integer  "user_id"
     t.string   "hashed_id"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
     t.string   "password"
     t.decimal  "current_total"
     t.boolean  "active",        default: true
+    t.boolean  "can_fund",      default: false
   end
 
   create_table "users", force: :cascade do |t|
